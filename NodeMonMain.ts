034f8cb9f6552c85ee175a96ddc68e5bbdc1c979
ShaunPark <coolage73@gmail.com>
2021-06-29 11:45:55 +0900

    20210629

--- NodeMonMain.ts
+++ NodeMonMain.ts
@@ -1,5 +1,5 @@
-import { exit } from "process";
-import { checkServerIdentity } from "tls";
+// import { exit } from "process";
+// import { checkServerIdentity } from "tls";
 import { parse } from "ts-command-line-args"
 import { IArguments, IConfig} from "./types/Type"
 import K8sMonitor from "./monitors/K8sMonitor";
---
+++ utils/AWSReboot.ts
@@ -0,0 +1,39 @@
+import * as AWS from 'aws-sdk';
+
+export class AWSReboot {
+  private ec2:any;
+
+  constructor() {
+    this.ec2 = new AWS.EC2();
+  }
+
+  public run() {
+    this.ec2.describeInstances({DryRun: true}, (err:any, data:any) => {
+      if (err) {
+        console.log("Error", err.stack);
+      } else {
+        console.log("Success", JSON.stringify(data));
+      }
+    })
+  }
+}
+
+// // var AWS = require('aws-sdk');
+// // // Set the region 
+// // AWS.config.update({region: 'REGION'});
+
+// // Create EC2 service object
+// var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+// var params = {
+//   DryRun: false
+// };
+
+// // Call EC2 to retrieve policy for selected bucket
+// ec2.describeInstances(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err.stack);
+//   } else {
+//     console.log("Success", JSON.stringify(data));
+//   }
+// });
