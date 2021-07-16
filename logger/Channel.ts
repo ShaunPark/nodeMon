f20cb1ebc0b322b9ce0fbeffef175c90fcdc0e06
ShaunPark <coolage73@gmail.com>
2021-07-16 10:45:46 +0900

    aaa

--- logger/Channel.ts
+++ logger/Channel.ts
@@ -2,29 +2,35 @@ import { MessagePort } from "worker_threads";
 import { ESLog } from "../exporters/ESExporter";
 import { NodeConditionCache } from "../managers/NodeManager";
 class Channel {
-    private static esPort:MessagePort;
-    private static nmPort:MessagePort;
+    private static esPort: MessagePort;
+    private static nmPort: MessagePort;
 
-    public static initLogger(esPort:MessagePort, nmPort:MessagePort) {
+    public static initLogger(esPort: MessagePort, nmPort: MessagePort) {
         Channel.esPort = esPort;
         Channel.nmPort = nmPort;
     }
 
-    public static initLoggerForNodeManager(esPort:MessagePort) {
+    public static initLoggerForNodeManager(esPort: MessagePort) {
         Channel.esPort = esPort;
     }
 
-    public static sendMessageEventToES(log:ESLog) {
-        const logJSON = JSON.parse(JSON.stringify(log))
-        Channel.esPort.postMessage({data:{log:logJSON, kind:"log"}});
+    public static sendMessageEventToES(log: ESLog) {
+        Channel.esPort.postMessage({ data: { log: log, kind: "log" } });
     }
 
-    public static sendNodeStatusToES(node:NodeConditionCache) {
-        const nodeJson = JSON.parse(JSON.stringify(node))
-        Channel.esPort.postMessage({data:{kind:"status", status:nodeJson}});
+    public static sendNodeStatusToES(node: NodeConditionCache) {
+        const nodeJson = {
+            ipAddress: node.ipAddress,
+            lastUpdateTime: node.lastUpdateTime,
+            status: node.status,
+            lastRebootedTime: node.lastRebootedTime,
+            nodeName: node.nodeName,
+            UUID: node.UUID
+        }
+        Channel.esPort.postMessage({ data: { kind: "status", status: nodeJson } });
     }
 
-    public static sendEventToNodeManager(event:any) {
+    public static sendEventToNodeManager(event: any) {
         Channel.nmPort.postMessage(event);
     }
 }
