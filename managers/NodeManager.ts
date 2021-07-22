3fd0d63a00854d9c4ec9027741e156a3748bc1e6
ShaunPark <coolage73@gmail.com>
2021-07-22 13:31:39 +0900

    aa

--- managers/NodeManager.ts
+++ managers/NodeManager.ts
@@ -464,7 +464,7 @@ export default class NodeManager {
             .sort((node1, node2) => {
                 const time1 = (node1.lastRebootedTime) ? node1.lastRebootedTime.getTime() : 0
                 const time2 = (node2.lastRebootedTime) ? node2.lastRebootedTime.getTime() : 0
-                return time2 - time1
+                return time1 - time2
             })
             .map(node => node.nodeName)
 
