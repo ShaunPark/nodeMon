7541f8713046bae74518d2ce67200b0d451af809
ShaunPark <coolage73@gmail.com>
2021-07-30 11:02:18 +0900

    aa

--- managers/NodeManager.ts
+++ managers/NodeManager.ts
@@ -183,9 +183,10 @@ export default class NodeManager {
     private printNode = () => {
         const arr = new Array<Object>()
         NodeStatus.getAll().forEach((node, key) => {
+            const lut = new Date(node.lastUpdateTime)
             arr.push({
                 name: key, ipAddress: node.ipAddress,
-                lastUpdateTime: node.lastUpdateTime,
+                lastUpdateTime: `${lut.toDateString()} ${lut.toTimeString()}`,
                 status: node.status, lastRebootedTime: new Date(node.lastRebootedTime),
                 hasScheduled: node.hasScheduled, hasReboodRequest: node.hasReboodRequest
             })
